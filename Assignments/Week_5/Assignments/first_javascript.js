b7bb8cc3ab78f62aab9260011f5e24bf4dd9b888
RL714 <70599451+RL714@users.noreply.github.com>
2020-09-30 15:08:08 -0500

    Please let this work

---
+++ Assignments/Week_5/Assignments/first_javascript.js
@@ -0,0 +1,37 @@
+console.log("Hello World!");
+console.log("How are you doing"); 
+
+//This is a single line comment
+/* 
+Always want to runaway 
+Always want to feel this way 
+
+Multi line comment
+*/ 
+
+//DATATYPES
+// STRING
+"this is a really long sentence. 76t7668t6tyg6t^&^"; 
+"To use single quotes ' and ' in the string, use double quotes on the outside.";
+'To use double quotes " and " in the string use single quotes on the outside.'
+
+// " Hello this is a double quote " Its used for text" 
+
+"100";
+
+console.log(1000); 
+
+console.log (true);
+
+console.log ("Hey there"); 
+
+// Number 
+100; 
+10.9; 
+.7009; 
+
+//You cannot do math on strings! 
+
+//Boolean 
+true;
+false; 
