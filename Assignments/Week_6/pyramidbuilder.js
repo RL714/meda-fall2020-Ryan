430a62330a270d5942ee7d0eec8d05470dd57316
RL714 <70599451+RL714@users.noreply.github.com>
2020-10-09 17:38:30 -0500

    Project 2

--- Assignments/Week_6/pyramidbuilder.js
+++ Assignments/Week_6/pyramidbuilder.js
@@ -1,3 +1,56 @@
+/*
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
+
+// console.log("*");
+// console.log("**");
+// console.log("***");
+
+// "Remembers" what we previously had, starts with no asterisks.
+var column = "";
+var pyramidHeight = 7;
+var pyramidCharacter = "}";
+
+
+
+
+
+for (var i = 0; i < pyramidHeight; i = i + 1) {
+
+    // We add one asterisk.
+    column = column + pyramidCharacter;
+
+    // We console.log what the column is currently holding.
+    console.log(column);
+}
+
+// console.log("final column state:", column);
+// var columnArray = column.split("");
+// console.log(columnArray);
+// columnArray.pop();
+// console.log(columnArray);
+// var updatedColumn = columnArray.join("");
+// console.log(updatedColumn);
+
+
+for (var i = 0; i < pyramidHeight; i = i + 1) {
+    var columnArray = column.split("");
+    columnArray.pop();
+    column = columnArray.join("");
+    console.log(column);
+}
+
+//MY BUNK ASS CODE 
 /* 
 
 *
@@ -17,36 +70,37 @@
 //console.log("***"); 
 
 
-var column = ""; 
+//var column = ""; 
 
-for (var i = 0; i < 10; i = i + 1) {
-    column = column + "*"; 
+//for (var i = 0; i < 10; i = i + 1) {
+    //column = column + "*"; 
   
-    console.log(column);   
+    //console.log(column);   
 
-console.log(column); 
+//console.log(column); 
 
-} 
+//} 
 
 //console.log("final column state", column); 
 
-var columnArray = column.split("");
+//var columnArray = column.split("");
 
 //console.log(columnArray); 
 
-columnArray.pop(); 
+//columnArray.pop(); 
 
 //console.log(columnArray);
 
-var updatedColumn = columnArray.joint("");
+//var updatedColumn = columnArray.joint("");
 
 //console.log(undatedColumn);
 
-for (ver i = 0; i < 10; i = i + 1) {
-    var columnArray = column.split(""); 
-    columnArray.pop(); 
-    column = columnArray.join(""); 
-}
+//for (ver i = 0; i < 10; i = i + 1) {
+//    var columnArray = column.split(""); 
+//    columnArray.pop(); 
+//    column = columnArray.join(""); 
+//    console.log(column); 
+//}
 
 
 
--- Assignments/Week_6/scopeandasync.js
+++ Assignments/Week_6/scopeandasync.js
@@ -1,101 +1,165 @@
-var timer = setInterval (function() {
-console.log("1 second has passed."); 
-}, 1000);  
+var timer = setInterval(function() { // Asynchronous
+    console.log("1 second has passed");
+}, 1000);
+
+// Synchronous
+console.log(1);
+console.log(2);
+console.log(3);
+
+
+// Asynchronous
+
+setTimeout(function () {
+    console.log(5);
+
+    clearInterval(timer);
+}, 5000);
+
+//var runsForever = setInterval(function () {
+//     console.log("3 seconds have passed.");
+// }, 3000);
+
+// clearInterval(runsForever); this will stop the interval.
+
+console.log(6);
+
+// Closures
+
+var myGlobalVariable = 100;
+
+function someFunction() {
+    var myLocalVariable = 10;
+
+    console.log(myLocalVariable);
+
+    console.log(myGlobalVariable);
+}
+
+someFunction();
+
+console.log(myGlobalVariable);
+
+// console.log(myLocalVariable); Cannot access myLocalVariable because it is in a child scope.
+
+
+
+// If statements do not have scopes.
+if (true) {
+    // Code to run
+} 
+
+if (false) {
+    // Code to run
+}
+
+
+// Example
+if (true) {
+    var createdVariable = 300;
+}
+
+console.log(createdVariable);
+
+//MY BUNK ASSS CODE
+//var timer = setInterval (function() {
+//console.log("1 second has passed."); 
+//}, 1000);  
 
 
 
 //Synchronous 
-console.log(1);
-console.log(2);
-console.log(3);
+//console.log(1);
+//console.log(2);
+//console.log(3);
 
 
 
 
 //Asynchronous 
 
-setTimeout(function () { 
-    console.log(5); 
+//setTimeout(function () { 
+    //console.log(5); 
 
-}, 5000); 
+//}, 5000); 
 
 //var runsForever = setInterval(function () { console.log("3 Seconds have passed."); 
 //}, 3000); 
 
-console.log(6); 
+//console.log(6); 
 
 // Closures 
 
-var myGlobalVariable = 100; 
+//var myGlobalVariable = 100; 
 
-function someFunction () {
-    var myLocalVariable = 10; 
+//function someFunction () {
+  //  var myLocalVariable = 10; 
 
-    console.log(myLocalVariable); 
+    //console.log(myLocalVariable); 
 
-}
+//}
 
-someFunction(); 
+//someFunction(); 
 
-console.log(myGlobalVariable)
+//console.log(myGlobalVariable)
 
 // console.log(myLocalVariable); Cannot access myLocalVariable because it is in a child scope 
 
 //GOES SOMEWHERE ELSE ###################################
-var string ="This is a sentence."; 
+//var string ="This is a sentence."; 
 
-var stringArray = string.split(" ");
+//var stringArray = string.split(" ");
 
-console.log(stringArray); 
+//console.log(stringArray); 
 
-stringArray.pop(); 
+//stringArray.pop(); 
 
-console.log(stringArray); 
+//console.log(stringArray); 
 
-var updatedString = stringArray.join("!");
+//var updatedString = stringArray.join("!");
 
-console.log(updatedString); 
+//console.log(updatedString); 
 //GOES SOMEWHERE ELSE ####################################
 
 
 // If statements do not have scopes/ 
-if (true) {
+//if (true) {
 
-}
+//}
 
-if (false) {
+//if (false) {
     // Code to Run 
-}
+//}
 
 
-if (true) { 
-    var createdVariable = 100; 
-}
+//if (true) { 
+  //  var createdVariable = 100; 
+//}
 
-console.log(createdVariable); 
+//console.log(createdVariable); 
 
-for (var i = 0; i < stringArray.length; i = i + 1) {
+//for (var i = 0; i < stringArray.length; i = i + 1) {
 
-    var currentLetter = stringArray[i];
+  //  var currentLetter = stringArray[i];
     
-        firstMartch = i; 
+    //    firstMartch = i; 
     //break; This will stop the loop once we find the first match. 
 
-    stringArray[i] = "E"; 
+//    stringArray[i] = "E"; 
 
-    if (currentLetter === "e") {
-        console.log("We found an e!");
-        continue; // This will ignore the following code and move on with the rest of the loop 
-    }
+  //  if (currentLetter === "e") {
+    //    console.log("We found an e!");
+      //  continue; // This will ignore the following code and move on with the rest of the loop 
+//    }
 
-    console.log("No e here!"); 
-}
+  //  console.log("No e here!"); 
+//}
 
-if (firstMatch === -1) {
-    console.log("We did not find any Es!")
+//if (firstMatch === -1) {
+  //  console.log("We did not find any Es!")
 
-} else {
+//} else {
 
-}
+//}
 
-console.log(stringArray.join("")); 
+//console.log(stringArray.join("")); 
